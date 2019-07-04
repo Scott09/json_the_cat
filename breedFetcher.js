35aaa8a03e50736a27c70701d2ebd09388d7ba3a
Scott Appleton <Scottappleton09@gmail.com>
2019-07-04 21:15:18 +0000

    added gitignore and installed npm

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,11 +6,21 @@ request(`https://api.thecatapi.com/v1/breeds/search/?q=${args[2]}`, function (er
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.
   // console.log(typeof body);
-  if (!error) {
+    if (error) {
+      console.log('error ', error);
+    }
+  
     const data = JSON.parse(body);
-    console.log(data[0]['description']);
-  } else {
-    console.log('error ', error);
-  }
+    const breed = data[0];
+    if (breed) { 
+      console.log(breed.description);
+    } else {
+      console.log(`Failed to find ${args[2]}`);
+    }
+
+
+  
+    
+
   
 });
